f9e2c5751e6fb16cdc6a89dcf7b6a7d6bce6d017
knowndeveloper <kemal.hlvc@gmail.com>
2023-10-04 17:44:13 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useMemo, useCallback } from "react";
 
-function App() {
+const App = () => {
+  console.log("App Rendered.");
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
+
+  const duplicate = () => {
+    [...new Array(10000000)].forEach((item) => {});
+
+    return count * 2;
+  };
+
+  const duplicateCount = duplicate();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>
+        Count: <code>{count}</code>
+      </p>
+      <button onClick={() => setCount((prevCount) => prevCount + 1)}>
+        Count Arttır
+      </button>
+      <p>
+        DuplicateCount: <code>{duplicateCount}</code>
+      </p>
+      <input type="text" onChange={(e) => setText(e.target.value)} />
+      <p>
+        Text: <code>{text}</code>
+      </p>
     </div>
   );
-}
+};
 
 export default App;
